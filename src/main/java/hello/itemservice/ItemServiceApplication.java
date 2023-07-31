98f98c4b5a9ede89d22c6ee49feae27799f5508b
qkrmekem <magicofclown@naver.com>
2023-07-31 23:55:00 +0900

    DB연동 테스트 복습

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -36,15 +36,18 @@ public class ItemServiceApplication {
 
 
 	//임베디드 DB
-	/*@Bean
-	@Profile("test")
-	public DataSource dataSource() {
-		log.info("메모리 데이터베이스 초기화");
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.h2.Driver");
-		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
-		return dataSource;
-	}*/
+	//
+//	@Bean
+//	@Profile("test") // 프로필이 테스트인 경우에만 적용
+//	public DataSource dataSource() {
+//		log.info("메모리 데이터베이스 초기화");
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.h2.Driver");
+//		// jdbc:h2:mem:db; 임베디드(메모리)모드로 동작하는 h2데이베이스를 사용할 수 있음
+//		// DB_CLOSE_DELAY=-1 임베디드 모드에서는 db커넥션 연결이 모두 끊어디면 데이터베이스도 종료되는데 이것을 방지하는 설정
+//		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+//		dataSource.setUsername("sa");
+//		dataSource.setPassword("");
+//		return dataSource;
+//	}
 }
